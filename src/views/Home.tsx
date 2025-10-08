9907a6d21f324f2c07d1c0d500be6aa26360b9fe
Juuhey <jujupantel@gmail.com>
2025-10-08 13:53:57 +0200

    fix homepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { IconButton, Link } from "@mui/material";
 
@@ -38,20 +38,6 @@ export default function Home() {
         <br />
         Une idée = un outil.
       </Typography>
-      <Button
-        variant="outlined"
-        size="large"
-        href="/projets"
-        sx={{
-          borderRadius: 8,
-          px: 4,
-          py: 1.5,
-          fontWeight: 600,
-          fontSize: "1.1rem",
-        }}
-      >
-        Voir mes projets
-      </Button>
       <Box sx={{ display: "flex", gap: 2, mt: 2 }}>
         <IconButton
           component={Link}
